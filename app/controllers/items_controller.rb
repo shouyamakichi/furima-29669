a13e2506a5417c28b7d38bf415c26086614f6e8a
shouyamakichi <houyama5610@gmail.com>
2020-09-09 16:56:39 +0900

    ユーザー管理機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+#  before_action : move_to_index, except: :index
+
   def index
     @items = Item.all
   end
@@ -8,6 +10,20 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @items = Item.create(items_params)
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+
+
+  # private
+  # def move_to_index
+    # unless user_sign_in?
+      # redirect_to action: :index
+    # end
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname, uniqueness: true
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
+
+  validates :password, format: {with: /\A[a-z0-9]+\z/}, length: {minimum: 6}
+  validates :email, uniqueness: true
 end

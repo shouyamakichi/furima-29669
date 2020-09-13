b10fac97f9fbb300288da1fab4202e884093e855
shouyamakichi <houyama5610@gmail.com>
2020-09-13 15:39:59 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update,]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -34,9 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find_by(id: params[:id])
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    end
   end
 
   private
